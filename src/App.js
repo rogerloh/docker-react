72cc57bbaa040c2b05137a9e591a2644a2888365
Roger Loh <rogerloh@Rogers-MacBook-Pro.local>
2020-03-22 19:56:18 +0800

    change app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           "DevOps CICD using Docker Github and Travis CI"
+          "How i can be an Expert using DevOps"
         </p>
         <a
           className="App-link"
