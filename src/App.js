c3603eff68efc4bcf5f96ffc4e6ea4f7b971db2b
Roger Loh <rogerloh@Rogers-MacBook-Pro.local>
2020-03-22 19:28:51 +0800

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed <code>src/App.js</code> and save to reload.
+          DevOps CICD using Docker Github and Travis CI
         </p>
         <a
           className="App-link"
