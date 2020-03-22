9e94e76dd1d233e32e66ff24b71dd03027e566b0
Roger Loh <rogerloh@Rogers-MacBook-Pro.local>
2020-03-22 19:39:38 +0800

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DevOps CICD using Docker Github and Travis CI
+          "DevOps CICD using Docker Github and Travis CI"
         </p>
         <a
           className="App-link"
